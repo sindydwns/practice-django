12737603a5ff108103b1363628179f50bddcca41
sindydwns <sindydwns@naver.com>
2024-01-01 00:29:04 +0900

    url alias

--- board/urls.py
+++ board/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'board'
+
 urlpatterns = [
     path('', views.index),
-    path('<int:question_id>/', views.detail)
+    path('<int:question_id>/', views.detail, name='detail')
 ]
