a5802f2797fe1f25ae7647eceac11e5556f0aeae
sindydwns <sindydwns@naver.com>
2024-01-03 23:44:23 +0900

    add chat app

--- config/asgi.py
+++ config/asgi.py
@@ -11,11 +11,20 @@ import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": None, # Just HTTP for now. (We can add other protocols later.)
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                chat.routing.websocket_urlpatterns
+            )
+        )
+    ),
 })
